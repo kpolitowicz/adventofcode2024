b95438e0d388a9f685238085fd8f78ee2851f5fc
Kamil Politowicz <kamilp@hey.com>
2024-11-30 21:29:48 -0500

    config: added ts

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,26 @@ module.exports = {
   extends: ["eslint:recommended", "prettier"],
   env: {
     node: true,
+    es2021: true,
   },
   overrides: [
     {
       files: ["*.test.js"],
       plugins: ["jest"],
       extends: ["plugin:jest/recommended"],
+    },
+    {
+      files: ["*.ts"],
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      ],
+      plugins: ["@typescript-eslint"],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: __dirname,
+      },
     },
   ],
 };
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,7 @@
 import globals from "globals";
 import jest from "eslint-plugin-jest";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
@@ -8,29 +10,49 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
 });
 
-export default [
-  ...compat.extends("eslint:recommended", "prettier"),
-  {
+export default [{
+    ignores: ["**/dist"],
+}, ...compat.extends("eslint:recommended", "prettier"), {
     languageOptions: {
-      globals: {
-        ...globals.node,
-      },
+        globals: {
+            ...globals.node,
+        },
     },
-  },
-  ...compat.extends("plugin:jest/recommended").map((config) => ({
+}, ...compat.extends("plugin:jest/recommended").map(config => ({
     ...config,
     files: ["**/*.test.js"],
-  })),
-  {
+})), {
     files: ["**/*.test.js"],
 
     plugins: {
-      jest,
+        jest,
     },
-  },
-];
+}, ...compat.extends(
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+).map(config => ({
+    ...config,
+    files: ["**/*.ts"],
+})), {
+    files: ["**/*.ts"],
+
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+        parser: tsParser,
+        ecmaVersion: 5,
+        sourceType: "script",
+
+        parserOptions: {
+            project: true,
+            tsconfigRootDir: "/Users/kamil/Projects/adventofcode2024",
+        },
+    },
+}];
