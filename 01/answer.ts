714c135d6f42db14c08dc4fc4e70b5ae1d45aa66
Kamil Politowicz <kamilp@hey.com>
2024-12-02 12:54:23 -0500

    config: added yargs

--- 01/answer.ts
+++ 01/answer.ts
@@ -4,16 +4,17 @@ const args = yargs
   .option("input", {
     alias: "i",
     description: "Input file",
+    default: `${__dirname}/input.txt`,
     demand: true,
   })
   .option("part", {
     alias: "p",
     description: "Answer part (1 or 2)",
     demand: true,
-  }).argv;
+  }).argv as { input: string; part: string };
 
 import fs from "fs";
-const input = fs.readFileSync(`./${args.input}`, "utf8");
+const input = fs.readFileSync(`${args.input}`, "utf8");
 
 import { firstAnswer, secondAnswer } from "./answerer";
 if (args.part == "1") {
