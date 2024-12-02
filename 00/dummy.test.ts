29a5d7c8269eb1600bef38d400513df584e50162
Kamil Politowicz <kamilp@hey.com>
2024-12-02 12:26:46 -0500

    config: removed palindromes code

--- 00/dummy.test.ts
+++ 00/dummy.test.ts
@@ -1,7 +1,7 @@
 import sum from "./dummy";
 
 describe("sum module", () => {
-  test("adds 1 + 2 to equal 3", () => {
+  it("adds 1 + 2 to equal 3", () => {
     expect(sum(1, 2)).toBe(3);
   });
 });
--- 00/palindromes.test.js
+++
@@ -1,7 +0,0 @@
-const palindromes = require("./palindromes");
-
-describe("palindromes()", () => {
-  it("correctly identifies one-word palindromes", () => {
-    expect(palindromes("madam")).toEqual(["madam"]);
-  });
-});
